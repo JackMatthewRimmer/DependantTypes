ed77764eb5ac2edb6168b774964601ea4c48c1ea
JackMatthewRimmer <rimbojnr@gmail.com>
2023-09-13 19:00:21 +0100

    Add start of matrix

--- dependant-types.hs
+++ dependant-types.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PolyKinds #-}
 
 import Data.Kind (Type)
 
@@ -14,7 +15,7 @@ intToNat n
   | n > 0     = Succ (intToNat (n - 1))
   | otherwise = error "intToNat is only defined for non-negative integers"
 
-data Vec :: Type -> Nat -> Type where
+data Vec :: * -> Nat -> * where
   Nil  :: Vec a 'Zero
   Cons :: a -> Vec a n -> Vec a ('Succ n)
 
@@ -28,3 +29,12 @@ showVec (Cons x xs) = "[" ++ show x ++ " : " ++ show xs ++ "]"
 vecSum :: Vec Int n -> Vec Int n -> Vec Int n
 vecSum (Cons a as) (Cons b bs) = Cons (a + b) (vecSum as bs) 
 vecSum Nil Nil = Nil
+
+data Matrix :: Nat -> Nat -> * -> * where
+  Matrix :: Nat -> Nat -> Vec (Vec Int n) m -> Matrix n m a 
+
+mat = Matrix (Succ Zero) (Succ Zero) (Cons (Cons 1 Nil) Nil)
+
+-- Define a Show instance for Matrix
+instance Show a => Show (Matrix n m a) where
+  show (Matrix _ _ rows) = showVec rows
