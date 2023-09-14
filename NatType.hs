f68444cea0c89e8a2e690f023e8be0cfdcaa221a
JackMatthewRimmer <rimbojnr@gmail.com>
2023-09-14 18:19:41 +0100

    Add basics of Nat arithmetic

--- NatType.hs
+++ NatType.hs
@@ -7,12 +7,30 @@
 module NatType where
 import Data.Kind (Type)
 
-
+-- Peano Reprisentation of Natual Numbers 
 data Nat = Zero | Succ ( Nat )
   deriving (Show)
 
-intToNat :: Int -> Nat
-intToNat 0 = Zero
-intToNat n
-  | n > 0     = Succ (intToNat (n - 1))
+-- defining num instance however some of the functions will be useless
+instance Num Nat where
+  n + m = n `natAdd` m
+  n * m = n `natMul` m
+  abs n = n
+  fromInteger = itn 
+
+natAdd :: Nat -> Nat -> Nat 
+natAdd Zero m = m
+natAdd n Zero = n
+natAdd (Succ n) (Succ m) = (Succ (Succ n)) `natAdd` m
+
+natMul :: Nat -> Nat -> Nat
+natMul Zero _ = Zero
+natMul _ Zero = Zero
+natMul (Succ n) m = m `natAdd` (n `natMul` m)
+
+-- itn == Int to Nat
+itn :: Integer -> Nat
+itn 0 = Zero
+itn n
+  | n > 0     = Succ (itn (n - 1))
   | otherwise = error "intToNat is only defined for non-negative integers"
